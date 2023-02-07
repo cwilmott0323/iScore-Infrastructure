a0a8cb566af3efca66d22ada1f74d3c812cf0c78
Chris Wilmott <admin@sosurprises.com>
2023-02-07 16:14:54 -0500

    update terraform

--- s3/terraform.tf
+++ s3/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     // This bucket is configured by terraform-iam.
-    bucket = "iscore-terraform-state"
+    bucket = "iscore-terraform-state-manager"
     key    = "terraform-s3/terraform.tfstate"
     region = "us-east-2"
   }
